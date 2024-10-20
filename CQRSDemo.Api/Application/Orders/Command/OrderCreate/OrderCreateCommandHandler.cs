4cba011360367c8f11e7ecd3310d7ad81bad445c
Orkhan <aliyevorhan82@gmail.com>
2024-10-21 02:04:53 +0400

    добавил хандлер

---
+++ CQRSDemo.Api/Application/Orders/Command/OrderCreate/OrderCreateCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using CQRSDemo.Api.Context;
+using CQRSDemo.Api.Entities;
+using MediatR;
+
+namespace CQRSDemo.Api.Application.Orders.Command.OrderCreate
+{
+    public class OrderCreateCommandHandler : IRequestHandler<OrderCreateCommand, Unit>
+    {
+        private readonly WriteDbContext _dbContext;
+        public OrderCreateCommandHandler(WriteDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+        public async Task<Unit> Handle(OrderCreateCommand request,
+            CancellationToken cancellationToken)
+        {
+            var order = new Order
+            {
+                CustomerId = request.CustomerId,
+                TotalAmount = request.TotalAmount,
+                OrderDate = request.OrderDate,
+                Status = request.Status
+            };
+            await _dbContext.AddAsync(order);
+            await _dbContext.SaveChangesAsync();
+
+            return Unit.Value;
+        }
+    }
+}
