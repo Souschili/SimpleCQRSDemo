89b2671e2fa06d204278620622753eb2b624c6c6
Orkhan <aliyevorhan82@gmail.com>
2024-10-20 23:51:33 +0400

    добавил команду для создания заказа

---
+++ CQRSDemo.Api/Application/Orders/Command/OrderCreate/OrderCreateCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace CQRSDemo.Api.Application.Orders.Command.OrderCreate
+{
+    public record class OrderCreateCommand(int CustomerId,
+        DateTime OrderDate,
+        decimal TotalAmount,
+        string Status) : IRequest<Unit>
+    { }
+
+}
