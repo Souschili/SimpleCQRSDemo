dcce9e448da4bc7919ca5488e53c32069e37c0e9
Orkhan <aliyevorhan82@gmail.com>
2024-10-21 22:00:14 +0400

    добавил второй контекст

---
+++ CQRSDemo.Api/Context/Factory/ReadDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CQRSDemo.Api.Context.Factory
+{
+    public class ReadDbContextFactory : IDesignTimeDbContextFactory<ReadDbContext>
+    {
+        public ReadDbContext CreateDbContext(string[] args)
+        {
+            // Загружаем конфигурацию из appsettings.json
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            
+            var optionsBuilder = new DbContextOptionsBuilder<ReadDbContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("ReadDb"));
+
+            return new ReadDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ CQRSDemo.Api/Context/Factory/WriteDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CQRSDemo.Api.Context.Factory
+{
+    public class WriteDbContextFactory : IDesignTimeDbContextFactory<WriteDbContext>
+    {
+        public WriteDbContext CreateDbContext(string[] args)
+        {
+            var config= new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<WriteDbContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("WriteDb"));
+            return new WriteDbContext(optionsBuilder.Options);
+        }
+    }
+}
